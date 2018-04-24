bbc5abb4aa22fc67fb80995baffbc22a28667ee7
Oluwaseun Ladeinde <oluwaseunladeinde@users.noreply.github.com>
2018-04-25 00:03:56 +0100

    Base Project

---
+++ yellowfeather/__init__.py
@@ -0,0 +1,9 @@
+__author__ = 'Ladeinde Oluwaseun'
+__copyright__ = 'Copyright 2018, Ladeinde Oluwaseun'
+__credits__ = ['Ladeinde Oluwaseun, amordigital']
+
+__license__ = 'BSD'
+__version__ = '1.5.1'
+__maintainer__ = 'Ladeinde Oluwaseun'
+__email__ = 'oluwaseun.ladeinde@gmail.com'
+__status__ = 'Development'
